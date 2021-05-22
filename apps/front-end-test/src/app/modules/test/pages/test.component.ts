1fd1e2b60d260ebd7a01564cdf6ed14b13f8450c
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-05-22 11:13:18 +0100

    ➕ Add test component to test module

---
+++ apps/front-end-test/src/app/modules/test/pages/test.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { GetTestDataService } from '../../../get-test-data.service';
+
+@Component({
+  selector: 'holition-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+  jsonData: JsonData;
+
+  constructor(private myService: GetTestDataService) {}
+
+  ngOnInit(): void {
+    this.myService.getTestData().subscribe((res: JsonData) => {
+      this.jsonData = res;
+      console.log(res.data);
+    });
+  }
+}
+// I used the interface of the json data to get intelisense suggestions
+interface JsonData {
+  data: {
+    imageUrl: string;
+    imageAlt: string;
+    imageTopText: string;
+    imageBottomText: string;
+    textSize1: string;
+    textSize2: string;
+    textSize3: string;
+  };
+}
--- apps/front-end-test/src/app/modules/test/test-routing.module.ts
+++ apps/front-end-test/src/app/modules/test/test-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './pages/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TestComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
